9a310f3398c79942cb4230cbdc3044f5c73ae095
Milos Ilic <milosilic89@outlook.com>
2024-01-08 22:54:55 +0100

    Starting the App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: 'amsterdam',
+    };
+
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  fetchWeather() {
+    console.log('Loading weather...');
+    console.log(this);
+  }
+
   render() {
     return (
-      <div>
+      <div className='app'>
         <h1>Classy Weather</h1>
         <div>
-          <input type='text' placeholder='City, Country' />
+          <input
+            type='text'
+            placeholder='City, Country'
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
         </div>
+        <button onClick={this.fetchWeather}>Get Weather</button>
       </div>
     );
   }
